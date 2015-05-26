3c8ac09c8a9508c8362e7ac4f88255ac249ec995
Raju <raju@newscred.com>
2015-05-26 10:45:10 -0400

    Fix bad words list

--- app/models/bad_word.rb
+++ app/models/bad_word.rb
@@ -1,7 +1,7 @@
 class BadWord
   #TODO populate this with a comprehensive list of bad words, possibly with a third party service
   #TODO match partially
-  BAD_WORDS = %w['foo, bar']
+  BAD_WORDS = %w['foo bar']
 
   def self.check(word)
     if BAD_WORDS.include? word
